fe9ca71d9cac6bec26e23378b4a64b1f169723f1
İbrahim Akgül <bilgislem@hotmail.com>
2023-04-20 04:10:07 +0300

    enhanced progress

--- InjectMee/InjectMee.cpp
+++ InjectMee/InjectMee.cpp
@@ -1,9 +1,14 @@
-// InjectMee.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define WIN32_LEAN_AND_MEAN
 
-#include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <experimental/filesystem>
+#include <string>
+#include <regex>
+
 
 DWORD GetPIDByName(const std::wstring& name) {
 	PROCESSENTRY32 pt;
@@ -60,6 +65,18 @@ int main()
 {
 	PROCESS_INFORMATION ProcInfo;
 	STARTUPINFOA StartupInfo;
+	char pInjectDLLPath[MAX_PATH];
+	char pCurrentPath[MAX_PATH];
+
+	ZeroMemory(&ProcInfo, sizeof(ProcInfo));
+	ZeroMemory(&StartupInfo, sizeof(StartupInfo));
+
+	GetCurrentDirectoryA(MAX_PATH, pCurrentPath);
+	sprintf_s(pInjectDLLPath, "%s\\%s", pCurrentPath, "streamer.dll");
+
+	if (!std::experimental::filesystem::exists(pInjectDLLPath))	{
+		ExitProcess;
+	}
 
 	const DWORD processID = GetPIDByName(L"streamer.exe");
 	if (processID == 0)	{
@@ -70,19 +87,11 @@ int main()
 		ProcInfo.dwProcessId = processID;
 	}
 
+	const char* ppInjectDLLPath = pInjectDLLPath;
+	const size_t dwInjectDLLLength = strlen(ppInjectDLLPath);
+
 	if (inject(ProcInfo.hProcess, 0)) {
 
 	}
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
